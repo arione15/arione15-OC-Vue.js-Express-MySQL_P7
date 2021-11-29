87621fcb668f19a2820d3be605ed8cce68fd03d2
Massine <massinoueslati@gmail.com156>
2021-11-29 15:57:41 +0100

    seeders pour Post

---
+++ .history/backend/seeders/20211129133114-create_fake_posts_20211129144816.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+
+
+
+
+
+
+        let posts = [];
+
+        for (let i = 0; i < 25; i++) {
+            let r = Math.random().toString(36).substring(7);
+            let pwdHashed = await bcrypt.hash(r, 10);
+            //let userRole = roles[(Math.random() * roles.length) | 0];
+            let userRole = roles[Math.floor(Math.random() * roles.length)];
+            users.push({
+                posterId: faker.random.uuid(),
+                title: faker.name.lastName(),
+                content: faker.internet.email(),
+                imageURL: faker.internet.url(),
+                videoURL: faker.internet.url(),
+                likes: faker.internet.url(),
+                dislikes: faker.internet.url(),
+                videoURL: faker.internet.url(),
+                createdAt: new Date(),
+            });
+        }
+        await queryInterface.bulkInsert('Posts', posts, {})
+    },
+
+    down: async(queryInterface, Sequelize) => {
+
+        await queryInterface.bulkDelete('Posts', null, {});
+
+    }
+};
---
+++ .history/backend/seeders/20211129133114-create_fake_posts_20211129145947.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        let posts = [];
+
+        for (let i = 0; i < 25; i++) {
+            const createPost = () => {
+                return {
+
+                    posterId: faker.random.uuid(),
+                    title: faker.lorem.sentence(),
+                    content: faker.lorem.sentence(),
+                    imageURL: faker.internet.url(),
+                    videoURL: faker.internet.url(),
+                    likes: faker.datatype.number({
+                        'min': 0,
+                        'max': 100
+                    }),
+                    dislikes: faker.datatype.number({
+                        'min': 0,
+                        'max': 100
+                    }),
+                    videoURL: faker.internet.url(),
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+
+                };
+            };
+
+            const createPosts = (numPosts = 25) => {
+                return Array.from({ length: numUsers }, createUser);
+            }
+            let fakePosts = createPosts(25);
+
+
+
+
+
+
+        }
+        await queryInterface.bulkInsert('Posts', posts, {})
+    },
+
+    down: async(queryInterface, Sequelize) => {
+
+        await queryInterface.bulkDelete('Posts', null, {});
+
+    }
+};
